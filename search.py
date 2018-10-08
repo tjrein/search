da3b4d3cf8aa6b6d21c75c313b1ecd4887e71d50
Tom Rein <tomjrein@gmail.com>
2018-10-08 02:15:17 -0400

    added initial logic for fetching all moves

--- search.py
+++ search.py
@@ -1,14 +1,77 @@
 from copy import deepcopy
 import sys
 
-def is_complete(state):
+def pprint(state):
     for row in state:
         print row
-        if -1 in row:
-            print "failing out"
+
+def add_pattern_to_move(piece_inds, pattern, state):
+
+    pprint(state)
+
+    for inds in piece_inds:
+        row, col = inds
+        new_location = [row + pattern[0], col + pattern[1]]
+        print "pattern", pattern
+        print "inds", inds
+        print "new location", new_location
+
+        cell_val = state[new_location[0]][new_location[1]]
+        print '\n'
+        print "cell_val", cell_val
+        print "state item", state[row][col]
+
+        if cell_val != 0 and cell_val != state[row][col]:
+            print "HOW YA DOING THEN"
             return False
 
-    print "puzzle complete"
+    return True
+         
+
+def check_possible_moves(piece, piece_inds, state):
+    directions = ('u', 'd', 'l', 'r')
+    possible_moves = []
+    row, col = piece_inds
+
+    move_patterns = {
+        'u': [-1, 0],
+        'd': [1, 0],
+        'l': [0, -1],
+        'r': [0, 1]
+    }
+
+    for direction in directions:
+        pattern = move_patterns[direction]
+
+        if add_pattern_to_move(piece_inds, pattern, state):
+            possible_moves.append((piece, direction))
+
+    print possible_moves
+
+def validate_move(state):
+    chosen_piece = 2
+    valid_moves = []
+
+    piece_inds = []
+
+    pprint(state)
+
+    for i, row in enumerate(state):
+        for j, piece in enumerate(row):
+            if piece == chosen_piece:
+                piece_inds.append((i, j))
+
+
+    check_possible_moves(chosen_piece, piece_inds, state)
+
+
+    return valid_moves
+
+def is_complete(state):
+    for row in state:
+        if -1 in row:
+            return False
+
     return True
 
 def display_state(dimensions, clone):
@@ -31,8 +94,9 @@ def load_file(filename):
 
     dimensions = initial_state.pop(0)
     clone = clone_state(initial_state)
-    display_state(dimensions, clone)
-    is_complete(clone)
+    #display_state(dimensions, clone)
+
+    validate_move(clone)
 
 def main():
     if len(sys.argv) > 1:
